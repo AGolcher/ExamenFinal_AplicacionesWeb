51f5b15056bda0ce89abbb1f1b093fd905cce3e4
GOLAUS <austin.golcher@gmail.com>
2022-08-16 20:30:34 -0600

    Examen terminado
    Se creo la página ejemplo, la cual tiene que ser accesada con su dirección en la Navbar

---
+++ ExamenFinal/src/main/java/com/examenFinal/ExanenFinalApplication.java
@@ -0,0 +1,13 @@
+package com.examenFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExanenFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExanenFinalApplication.class, args);
+	}
+
+}
